6065524d11fdfe08db17fd36e560e1f760a888b8
Vishnu Mavilla <vishnumavilla112@gmail.com>
2026-08-04 22:29:27 +0530

    Module2-Topic1-tasks

---
+++ track/M02-introduction-to-python/T01-introduction-to-python-basic-constructs/src/src/pseudocode_vs_python.py
@@ -0,0 +1,11 @@
+# Basic PseudoCode
+""" PRINT "welcome to python" 
+    PRINT "Python Track has started"
+    PRINT "Let's begin coding" """
+
+
+
+# Python Code
+print("welcome to python")
+print("Python Track has started")
+print("Let's begin coding")
---
+++ track/M02-introduction-to-python/T01-introduction-to-python-basic-constructs/src/src/simple_message.py
@@ -0,0 +1,4 @@
+# Print the first message. 
+print("I am learning python")
+# Print the second message. 
+print("Python is fun")
