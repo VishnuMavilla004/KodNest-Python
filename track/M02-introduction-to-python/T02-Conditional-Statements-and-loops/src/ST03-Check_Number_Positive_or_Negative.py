b0afa5679cb8608c76952b65b947769bbf4bbd32
Vishnu Mavilla <vishnumavilla112@gmail.com>
2026-08-06 08:59:38 +0530

    Checking a number is POsitive or Negative task using if-else conditiions

---
+++ track/M02-introduction-to-python/T02-Conditional-Statements-and-loops/src/ST03-Check_Number_Positive_or_Negative.py
@@ -0,0 +1,9 @@
+# Read the number by user input
+number = int(input("Enter any number: "))
+# Check whether the number is positive or negative
+if number > 0:
+    print("Number is positive")
+elif number < 0:
+    print("Number is negative")
+else:
+    print("Number is zero")
