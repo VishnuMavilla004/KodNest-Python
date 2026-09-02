c10126df03d33b8ec633895e69f10c77564f6d29
Vishnu Mavilla <vishnumavilla112@gmail.com>
2026-09-02 08:38:57 +0530

    Built a MatchScoreCalculator using Child Classes

---
+++ track/M03-Object_Oriented_Programming/T03-Inheritance,super()_and_Method_Overriding/ST19-Create_MatchScoreCalculator.py
@@ -0,0 +1,22 @@
+class SkillAnalyzer:
+    def __init__(self, student_skills, required_skills):
+        self.student_skills = set(student_skills)
+        self.required_skills = set(required_skills)
+
+    def get_matched_skills(self):
+        return self.student_skills & self.required_skills
+
+
+class MatchScoreCalculator(SkillAnalyzer):
+    # Add calculate_match_score()
+    def calculate_match_score(self):
+        return (len(self.get_matched_skills()) / len(self.required_skills))*100
+
+
+student_skills = input().split()
+required_skills = input().split()
+
+# Create the calculator and display the score
+calculator = MatchScoreCalculator(student_skills, required_skills)
+
+print(f"Match Score: {calculator.calculate_match_score():.2f}%")
