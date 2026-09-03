2dd261177d6f5de4cd54a390d3417b59c460f010
Vishnu Mavilla <vishnumavilla112@gmail.com>
2026-09-03 08:06:55 +0530

    Built an Abstract Payment Processor

---
+++ track/M03-Object_Oriented_Programming/T04-Abstraction,Polymorphism_and_Duck_Typing/ST12-Abstract_Payment_Processor.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+
+class PaymentProcessor(ABC):
+    # Add abstract process_payment()
+    @abstractmethod
+    def process_payment(self):
+        pass
+
+
+class UPIPayment(PaymentProcessor):
+    def __init__(self, amount):
+        self.amount = amount
+
+    # Implement process_payment()
+    def process_payment(self):
+        print(f"UPI Payment: {self.amount}")
+        
+
+
+amount = int(input())
+
+# Create the object and process the payment
+upi = UPIPayment(amount)
+upi.process_payment()
